013632965342ed90cf80a5e84efc8a61481596b9
Yassineelghizi <yassine.ghizi2@gmail.com>
2022-11-05 15:01:21 +0000

    new migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,10 +14,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
+            $table->string('address', 255)->nullable();
+            $table->unsignedBigInteger('role_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_pic', 255)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2022_10_08_195804_create_reservations_table.php
+++ database/migrations/2022_10_08_195804_create_reservations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
+            $table->date('allocation_date');
+            $table->unsignedBigInteger('scooter_id');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('duration');
+            $table->boolean('confirmed')->default(0);
+            $table->string('cin_front' , 254);
+            $table->string('cin_back' , 254);
+            $table->string('user_tel' , 255);
+
             $table->timestamps();
         });
     }
