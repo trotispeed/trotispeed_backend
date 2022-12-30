85f3c156e7ea2500836722a562b7568b73afc043
Yassineelghizi <yassine.ghizi2@gmail.com>
2022-12-30 18:06:55 +0000

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
 
-    public function register(Request $request){
-        return $request;
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+        
+        $user = User::query()->where('name', 'like', $request->name)->first();
+        $credentials = $request->only('name', 'password');
+        $data = [];
+        if (Auth::attempt($credentials)) {
+            $data['message'] = "Success This Change saved tt0363143";
+            return response()->json($data, 200);
+        } else {
+            return response()->json($data, 400);
+        }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/register' , [AuthController::class , 'register']);
+
