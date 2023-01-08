3c70a44f2afff6171419d2e6770794cf3b3ea1d3
Yassineelghizi <yassine.ghizi2@gmail.com>
2023-01-08 16:03:57 +0000

    register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 
 class AuthController extends Controller
@@ -16,7 +17,7 @@ class AuthController extends Controller
             'name' => 'required',
             'password' => 'required'
         ]);
-        
+
         $user = User::query()->where('name', 'like', $request->name)->first();
         $credentials = $request->only('name', 'password');
         $data = [];
@@ -26,6 +27,34 @@ class AuthController extends Controller
         } else {
             return response()->json($data, 400);
         }
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'name' => 'string|required',
+            'password' => 'string|required'
+        ]);
+
+        $user = User::query()->where('name', $request->name)
+            ->first();
+        $data = [];
+
+        if ($user == null) {
+            $u = User::create([
+                'name' => $request->name,
+                'email' => $request->name . '@gmail.com',
+                'password' => Hash::make($request->password),
+                'role_id' => 2
+            ]);
+            $data['message'] = 'User Has Been Created';
+            $data['user'] = $u;
+            return response()->json($data, 201);
+        } else {
+            $data['message'] = 'User Already Exists';
+            return response()->json($data, 400);
+        }
+
     }
 
 }
